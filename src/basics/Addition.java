77e5ee9d48eac5bc63825447a0af42bbba9c51b5
Nitin Chamber <nitinchamber@Nitins-MacBook-Air.local>
2024-08-25 15:47:40 +0530

    first commit

---
+++ src/basics/Addition.java
@@ -0,0 +1,20 @@
+package basics;
+
+import java.util.Scanner;
+
+public class Addition
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first integer: ");
+        int fNum = sc.nextInt();
+
+        System.out.print("Enter second integer: ");
+        int sNum = sc.nextInt();
+
+        int sum = fNum + sNum;
+        System.out.println("Sum of both integers = " + sum);
+    }
+}
